cf2e2359039e42246624559da2f7ea3d3b40ad0b
ojotuk <ojotuk14@yahoo.com>
2020-11-07 22:34:40 +0100

    reset css properties

--- components/Header.js
+++ components/Header.js
@@ -13,48 +13,7 @@ const Header = () => {
     setOpen(!open);
   };
 
-  return (
-    <div className={styles.navContainer}>
-      <div className={styles.brand}>
-        <img src="/iqlogo.png" alt="brand-logo" />
-        <span>IQ-Evaluate</span>
-      </div>
-      <ul className={styles.closed} ref={navLinks}>
-        <li className={styles.firstChild}>
-          <Link href="/">
-            <a> My Evaluation</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Services</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Pricing</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Features</a>
-          </Link>
-        </li>
-      </ul>
-      <span className={styles.logins}>
-        <Link href="/">
-          <a>Login / Sign Up</a>
-        </Link>
-      </span>
-      <span className={styles.toggler} onClick={toggler}>
-        {open ? (
-          <FontAwesomeIcon icon={faTimes} size="3x" />
-        ) : (
-          <FontAwesomeIcon icon={faBars} size="3x" />
-        )}
-      </span>
-    </div>
-  );
+  return <div className={styles.nav_bar}></div>;
 };
 
 export default Header;
